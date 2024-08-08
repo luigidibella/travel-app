0b2cfc9e78eb49fadf89f1e63ac2bb69fafcc072
LuigiDiBella <lykos.omega@gmail.com>
2024-08-08 16:13:17 +0200

    [add] Navbar

---
+++ src/components/_Navbar.jsx
@@ -0,0 +1,17 @@
+import './navbar.css'
+
+function Navbar () {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Dettaglio</a></li>
+          <li><a href="#">Mappa</a></li>
+        </ul>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar
