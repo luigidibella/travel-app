5d8feb49e5a219845664995dd1a6d90e0eb71f24
LuigiDiBella <lykos.omega@gmail.com>
2024-08-09 16:29:54 +0200

    [add] Calendario page

---
+++ src/pages/Calendario.jsx
@@ -0,0 +1,12 @@
+import Navbar from '../components/Navbar';
+
+function Calendario () {
+  return (
+    <>
+      <Navbar></Navbar>
+      <h1>Calendario</h1>
+    </>
+  );
+}
+
+export default Calendario;
