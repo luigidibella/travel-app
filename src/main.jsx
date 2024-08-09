eaa361b059225bb6cf9e033060cb1c128b36e136
LuigiDiBella <lykos.omega@gmail.com>
2024-08-09 16:15:13 +0200

    [add] router

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,38 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ListaViaggi from "./pages/ListaViaggi.jsx";
+import Mappa from "./pages/Mappa.jsx";
+import Calendario from "./pages/Calendario.jsx";
+
 import store from './redux/store.js';
 import { Provider } from 'react-redux';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>
+  },
+  {
+    path: "/lista-viaggi",
+    element: <ListaViaggi></ListaViaggi>
+  },
+  {
+    path: "/mappa",
+    element: <Mappa></Mappa>
+  },
+  {
+    path: "/calendario",
+    element: <Calendario></Calendario>
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      {/* <App></App> */}
+      <RouterProvider router={router} />
     </Provider>
   </StrictMode>,
 );
