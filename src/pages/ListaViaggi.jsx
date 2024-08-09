457bcccab3d89a9f9bba466b029678e0e96c5ee0
LuigiDiBella <lykos.omega@gmail.com>
2024-08-09 16:29:21 +0200

    [add] ListaViaggi page

---
+++ src/pages/ListaViaggi.jsx
@@ -0,0 +1,12 @@
+import Navbar from '../components/Navbar';
+
+function ListaViaggi () {
+  return (
+    <>
+      <Navbar></Navbar>
+      <h1>Lista Viaggi</h1>
+    </>
+  );
+}
+
+export default ListaViaggi;
