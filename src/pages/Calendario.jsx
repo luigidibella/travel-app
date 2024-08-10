eec7119471f6516e3cf3756bb2d4232a348836c2
LuigiDiBella <lykos.omega@gmail.com>
2024-08-10 19:08:36 +0200

    [edit] Calendario page

--- src/pages/Calendario.jsx
+++ src/pages/Calendario.jsx
@@ -2,10 +2,15 @@ import Navbar from '../components/Navbar';
 
 function Calendario () {
   return (
-    <>
-      <Navbar></Navbar>
-      <h1>Calendario</h1>
-    </>
+    <div className="flex flex-col min-h-screen"> {/* Flex container che occupa tutta la schermata */}
+      <Navbar />
+      <div className="flex-grow"> {/* Div che cresce per riempire lo spazio restante */}
+        <h1 className="text-center text-2xl font-bold my-4">Calendario</h1>
+        <div className="flex justify-center items-center h-full">
+          <img src="src/assets/img/calendario-placeholder.png" alt="Calendario placeholder" className="max-w-full max-h-full object-contain" />
+        </div>
+      </div>
+    </div>
   );
 }
 
