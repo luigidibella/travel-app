e293e5a6d473af989fc825579cbaf17f4ee72ff6
LuigiDiBella <lykos.omega@gmail.com>
2024-08-10 19:08:07 +0200

    [edit] Mappa page

--- src/pages/Mappa.jsx
+++ src/pages/Mappa.jsx
@@ -1,11 +1,16 @@
 import Navbar from '../components/Navbar';
 
-function Mappa () {
+function Mappa() {
   return (
-    <>
-      <Navbar></Navbar>
-      <h1>Mappa</h1>
-    </>
+    <div className="flex flex-col min-h-screen"> {/* Flex container che occupa tutta la schermata */}
+      <Navbar />
+      <div className="flex-grow"> {/* Div che cresce per riempire lo spazio restante */}
+        <h1 className="text-center text-2xl font-bold my-4">Mappa</h1>
+        <div className="flex justify-center items-center h-full">
+          <img src="src/assets/img/mappa-placeholder.jpg" alt="Mappa placeholder" className="max-w-full max-h-full object-contain" />
+        </div>
+      </div>
+    </div>
   );
 }
 
