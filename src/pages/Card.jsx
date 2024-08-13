3d1f0a62c96a4024bf1e6abfd96548055b78ba97
LuigiDiBella <lykos.omega@gmail.com>
2024-08-13 14:01:23 +0200

    [add] back button to Card page

--- src/pages/Card.jsx
+++ src/pages/Card.jsx
@@ -1,5 +1,5 @@
 import Navbar from '../components/Navbar';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 function Card () {
@@ -17,6 +17,12 @@ function Card () {
       <Navbar />
       <div className="flex-grow"> {/* Div che cresce per riempire lo spazio restante */}
         <h1 className="text-center text-2xl font-bold my-4">Dettaglio Viaggio</h1>
+        <Link 
+          to={'/lista-viaggi'} 
+          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          indietro
+        </Link>
         <div className="mt-5 flex bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
           <a href="#">
             <div className="square">
