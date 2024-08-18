ce1f08807f152bb61eb7e003012bc9b057834b60
LuigiDiBella <lykos.omega@gmail.com>
2024-08-18 20:49:00 +0200

    [add] react-calendar

--- src/pages/Calendario.jsx
+++ src/pages/Calendario.jsx
@@ -1,4 +1,6 @@
 import Navbar from '../components/Navbar';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 function Calendario () {
   return (
@@ -6,7 +8,8 @@ function Calendario () {
       <Navbar />
       <div className="flex-grow"> {/* Div che cresce per riempire lo spazio restante */}
         <h1 className="text-center text-2xl font-bold my-4">Calendario</h1>
-        <div className="flex justify-center items-center h-full">
+        <div className="flex flex-col justify-center items-center h-full">
+          <Calendar></Calendar>
           <img src="src/assets/img/calendario-placeholder.png" alt="Calendario placeholder" className="max-w-full max-h-full object-contain" />
         </div>
       </div>
