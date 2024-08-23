c3d1e6825c35ed2185694fe4f227693e45ccad24
LuigiDiBella <lykos.omega@gmail.com>
2024-08-23 14:00:31 +0200

    [install] firebase, tomtom.

---
+++ src/firebaseConfig.js
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCJo2WwJSt9QL75fXEtihOzs8xEBhSm2CU",
+  authDomain: "travel-app-1set24.firebaseapp.com",
+  projectId: "travel-app-1set24",
+  storageBucket: "travel-app-1set24.appspot.com",
+  messagingSenderId: "687633030692",
+  appId: "1:687633030692:web:62d36019c065949f4bc2b1",
+  measurementId: "G-2XXYVGKY83"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Initialize Firebase services
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { app, auth, db, storage };
