7a13a5f7113885e2151b5c3f6337f1c64c691312
LuigiDiBella <lykos.omega@gmail.com>
2024-08-28 00:55:54 +0200

    [add] Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer () {
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center h-full mt-5">
+        <img src="/assets/img/cover.jpg" alt="cover" className="max-w-full max-h-full object-contain" />
+      </div>
+    </>
+  );
+}
+
+export default Footer;
