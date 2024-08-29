89a8fedf3a0089506310a68d3199fe01db9aefaa
LuigiDiBella <lykos.omega@gmail.com>
2024-08-29 23:24:23 +0200

    [edit] removeStage() margin

--- src/components/CardForm.jsx
+++ src/components/CardForm.jsx
@@ -196,7 +196,7 @@ function CardForm() {
       {formData.stages.length > 0 && (
         <ol className="relative border-s border-gray-200 dark:border-gray-700 mt-5">
           {formData.stages.map((stage, index) => (
-            <li key={index} className="mb-10 ms-4">
+            <li key={index} className="mb-10 relative">
               <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
               <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">{formatDate(stage.date)}</time>
               <h3 className="text-lg font-semibold text-gray-900 dark:text-white">{stage.name}</h3>
