c8eb8126149ed7bade675693edac0fe1b9f9f42f
LuigiDiBella <lykos.omega@gmail.com>
2024-09-01 15:47:31 +0200

    [add] editId prop

--- src/pages/EditTravel.jsx
+++ src/pages/EditTravel.jsx
@@ -12,7 +12,7 @@ function EditTravel() {
 
   const location = useLocation();
   const { city } = location.state || {};
-
+  
   console.log(city);
   
   return (
@@ -24,6 +24,7 @@ function EditTravel() {
           <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <CardForm 
+                editId={city.id} 
                 initialData={city} 
               />
             </div>
